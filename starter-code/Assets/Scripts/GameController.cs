a674c72196a5e19dc6ebc36c623d9791abf1c207
Jacob-art-sketch <63834128+Jacob-art-sketch@users.noreply.github.com>
2023-03-29 08:14:02 +1300

    adding all the games

---
+++ starter-code/Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(MazeConstructor))]
+
+public class GameController : MonoBehaviour
+{
+    private MazeConstructor constructor;
+    public GameObject playerPrefab;
+    public GameObject monsterPrefab;
+    private void CreatePlayer()
+    {
+        Vector3 playerStartPosition = new Vector3(constructor.hallWidth, 1, constructor.hallWidth);
+        GameObject player = Instantiate(playerPrefab, playerStartPosition, Quaternion.identity);
+        player.tag = "Generated";
+        Vector3 vert1 = new Vector3(-.5f, -.5f, 0);
+        Vector3 vert2 = new Vector3(-.5f, .5f, 0);
+        Vector3 vert3 = new Vector3(.5f, .5f, 0);
+        Vector3 vert4 = new Vector3(.5f, -.5f, 0);
+    }
+    void Awake()
+    {
+        constructor = GetComponent<MazeConstructor>();
+    }
+
+    void Start()
+    {
+        constructor.GenerateNewMaze(rows, cols);
+        CreatePlayer();
+        CreateMonster();
+    }
+    private void CreateMonster()
+    {
+        Vector3 monsterPosition = new Vector3(constructor.goalCol * constructor.hallWidth, 0f, constructor.goalRow * constructor.hallWidth);
+        GameObject monster = Instantiate(monsterPrefab, monsterPosition, Quaternion.identity);
+        monster.tag = "Generated";
+    }
+}
